07d5fdb5e8e74a89ac288ecfe6856e49c20e3adb
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-05-22 17:12:45 +0900

    first version of the first screen - no logic yet

--- UmbrellaSharing-iOS/ViewController.swift
+++ UmbrellaSharing-iOS/ViewController.swift
@@ -10,9 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var rentPriceHeaderLabel: UILabel!
+    @IBOutlet weak var firstOptionRentLabel: UILabel!
+    @IBOutlet weak var secondOptionRentLabel: UILabel!
+    @IBOutlet weak var thirdOptionRentLabel: UILabel!
+    @IBOutlet weak var firstOptionRentPriceLabel: UILabel!
+    @IBOutlet weak var secondOptionRentPriceLabel: UILabel!
+    @IBOutlet weak var thirdOptionRentPriceLabel: UILabel!
+    @IBOutlet weak var buyPriceHeaderLabel: UILabel!
+    @IBOutlet weak var firstOptionBuyLabel: UILabel!
+    @IBOutlet weak var firstOptionBuyPriceLabel: UILabel!
+    
+    @IBOutlet weak var rentButton: UIButton!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var checkLocationsButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initInterface()
+    }
+    
+    private func initInterface() {
+        // TODO: Exctract all constant string to the separate file
+        // TODO: Check if all versions of iPhone follow the design
+        
+        // MARK: Init labels
+        
+        rentPriceHeaderLabel.text = "Rental Rate:"
+        firstOptionRentLabel.text = "Less than an hour"
+        firstOptionRentPriceLabel.text = "50 rub"
+        secondOptionRentLabel.text = "Less than a day"
+        secondOptionRentPriceLabel.text = "100 rub"
+        thirdOptionRentLabel.text = "More than a day"
+        thirdOptionRentPriceLabel.text = "300 rub"
+        buyPriceHeaderLabel.text = "Buy Rate:"
+        firstOptionBuyLabel.text = "Buy an umbrella"
+        firstOptionBuyPriceLabel.text = "300 rub"
+        
+        // MARK: Init buttons
+        rentButton.setTitle("Rent an Umbrella", for: .normal)
+        buyButton.setTitle("Buy an Umbrella", for: .normal)
+        
+        rentButton.layer.cornerRadius = 20
+        rentButton.layer.borderWidth = 1
+        rentButton.layer.borderColor = UIColor.black.cgColor
+        
+        buyButton.layer.cornerRadius = 20
+        buyButton.layer.borderWidth = 1
+        buyButton.layer.borderColor = UIColor.black.cgColor
+        
+        checkLocationsButton.setTitle("Check Locations on the Map", for: .normal)
+        // TODO: Set an image for this button
+        rentButton.setTitle("Rent an Umbrella", for: UIControl.State.normal)
+    
+        
     }
 
 
