ea9d56af77a99ac836a954c925367014e326d1ad
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-05-22 14:43:17 +0900

    first commit

---
+++ UmbrellaSharing-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UmbrellaSharing-iOS
+//
+//  Created by Katselenbogen, Igor on 2020/05/22.
+//  Copyright © 2020 Katselenbogen, Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
