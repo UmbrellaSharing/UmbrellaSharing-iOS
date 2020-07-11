6840176c4f1d902c9b5714902aabab7db0177f97
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-07-11 17:34:59 +0900

    added order Entity and order DTO objects

--- UmbrellaSharing-iOS/Entities/OrderEntity.swift
+++ UmbrellaSharing-iOS/Entities/OrderEntity.swift
@@ -1,15 +1,12 @@
 //
-//  OrderInformation.swift
+//  OrderEntity.swift
 //  UmbrellaSharing-iOS
 //
 //  Created by Katselenbogen, Igor on 2020/07/11.
 //  Copyright © 2020 Katselenbogen, Igor. All rights reserved.
 //
 
-import Foundation
-
-struct OrderInformation {
-    var userId: Int?
-    var orderId: Int?
-    var code: Int?
+struct OrderEntity: Decodable {
+    var orderId: Int
+    var code: Int
 }
