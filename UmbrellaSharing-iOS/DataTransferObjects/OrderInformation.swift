3c263512b6a63960d3bd8c038614255b8ed8aa66
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-07-11 12:24:11 +0900

    init git ignore file

---
+++ UmbrellaSharing-iOS/DataTransferObjects/OrderInformation.swift
@@ -0,0 +1,9 @@
+//
+//  OrderInformation.swift
+//  UmbrellaSharing-iOS
+//
+//  Created by Katselenbogen, Igor on 2020/07/11.
+//  Copyright © 2020 Katselenbogen, Igor. All rights reserved.
+//
+
+import Foundation
