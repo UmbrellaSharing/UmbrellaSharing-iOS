e547284807e1c306442775a80075b682845d86b9
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-07-13 22:34:51 +0900

    created QR View Model

---
+++ UmbrellaSharing-iOS/ViewModels/QRViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  QRViewModel.swift
+//  UmbrellaSharing-iOS
+//
+//  Created by Katselenbogen, Igor on 2020/07/13.
+//  Copyright © 2020 Katselenbogen, Igor. All rights reserved.
+//
+
+import Foundation
+
+class QRViewModel {
+    
+    func canWeProceed(orderId: Int, qrType: UmbrellaUtil.OperationType) -> Bool {
+        // TODO: This suppose to call API "/Order/CanGoFurther" - When Iliya fix the API
+        return true
+    }
+    
+}
