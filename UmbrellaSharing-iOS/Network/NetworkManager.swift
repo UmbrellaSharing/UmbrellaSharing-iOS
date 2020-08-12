6c08a6d4804ac0299699fd3e6d9b58998e016ef9
Igor Katselenbogen <igor.katselenbogen@rakuten.com>
2020-08-12 11:55:31 +0900

    Implemented all API calls

--- UmbrellaSharing-iOS/Network/NetworkManager.swift
+++ UmbrellaSharing-iOS/Network/NetworkManager.swift
@@ -12,7 +12,6 @@ import PromiseKit
 class NetworkManager {
     
     // TODO: Level 3 - Exctract all path in a config file or so
-    // TODO: Level 1 - Implement all API calls
     // TODO: Level 3 - Make all validatoin and error check inhere
     
     // Usefull links for making URL sessions. Please use them while working on this class
@@ -31,7 +30,7 @@ class NetworkManager {
         self.baseURL = URL(string: pathBaseURL)!
     }
     
-    // MARK: Fetch QR Code
+    // MARK: Get QR Code to Rent or To Buy an Umbrella
     // Type: GET
     func getQRCodeInformation(userId: Int, isBuy: Bool) -> Promise<OrderEntity> {
         let pathComponent = "order/getQrCodeToTake"
@@ -45,10 +44,56 @@ class NetworkManager {
         }
     }
 
-    // MARK: GET ORDER INFO
-    // MARK: GET QR CODE TO PASS
-    // MARK: SAVE FEEDBACK
-    // MARK: CAN GO FURTHER
+    // MARK: Get QR Code to Return an Umbrella
+    // Type: Get
+    func getReturnQRCodeInformation(orderId: Int) -> Promise<ReturnEntity> {
+        let pathComponent = "order/getQrCodeToPass"
+        let requestURL = self.baseURL.appendingPathComponent(pathComponent)
+            .appending("orderId", value: String(orderId))
+        return firstly {
+            URLSession.shared.dataTask(.promise, with: requestURL)
+        } .compactMap {
+            return try JSONDecoder().decode(ReturnEntity.self, from: $0.data)
+        }
+    }
+    
+    // MARK: Save User's Feedback
+    //Type: POST
+    // TODO: Level 2 After Iliya check the server, test this part, since for right now it returns 404 error
+    // TODO: Level 2 Think about how it could be implemented in more consistent way
+    func postFeedback(orderId: Int, feedback: String, mark: Int) {
+        let requestURL = self.baseURL.appendingPathComponent("order/saveFeedback")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let json = [
+            "orderId": String(orderId),
+            "feedback": feedback,
+            "mark": String(mark)
+        ]
+        // TODO: Level 3 - Handle this serialization error better
+        let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+        let task = URLSession.shared.uploadTask(with: request, from: jsonData) { _, _, error in
+            if let error = error {
+                print("Error: \(error)")
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: Asking The Server If QR Code Was Scanned
+    // Type: Get
+    func getCanGoFurther(orderId: Int, qrType: Int) -> Promise<PassEntity> {
+        let pathComponent = "order/canGoFurther"
+        let requestURL = self.baseURL.appendingPathComponent(pathComponent)
+            .appending("orderId", value: String(orderId))
+            .appending("qrType", value: String(qrType))
+        return firstly {
+            URLSession.shared.dataTask(.promise, with: requestURL)
+        } .compactMap {
+            return try JSONDecoder().decode(PassEntity.self, from: $0.data)
+        }
+    }
     
     // MARK: Add New User
     // Type: GET
@@ -61,7 +106,16 @@ class NetworkManager {
         }
     }
     
-    // MARK: GET RATE PLAN
+    // MARK: Get The Information About Rate Plan
+    // Type: Get
+    func getRatePlan() -> Promise<[RatePlanEntity]> {
+        let requestURL = self.baseURL.appendingPathComponent("data/getRatePlan")
+        return firstly {
+            URLSession.shared.dataTask(.promise, with: requestURL)
+        }.compactMap {
+            return try JSONDecoder().decode([RatePlanEntity].self, from: $0.data)
+        }
+    }
     
     // MARK: Fetch All Frequently Asked Question
     // Type: GET
